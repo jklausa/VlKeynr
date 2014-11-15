8ccdb88084f2c1022e9486845f1ae5a35aa213b5
Michał Kałużny <michalkaluzny@gmail.com>
2014-11-15 12:08:05 +0100

    Initial repo config

---
+++ VlKeynr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VlKeynr
+//
+//  Created by Michał Kałużny on 15.11.2014.
+//  Copyright (c) 2014 Michał Kałużny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
