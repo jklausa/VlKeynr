f8e6c29d424f9e9051bf3f5ab3746dd531fd0d2d
Michał Kałużny <michalkaluzny@gmail.com>
2014-11-18 21:09:59 +0100

    Added the actual iOS app target with basic implementation

--- VlKeynr/ViewController.swift
+++ VlKeynr/ViewController.swift
@@ -7,19 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: VlKeynrButton!
+    @IBOutlet weak var button2: VlKeynrButton!
+    @IBOutlet weak var button3: VlKeynrButton!
+    @IBOutlet weak var button4: VlKeynrButton!
+    @IBOutlet weak var button5: VlKeynrButton!
+    @IBOutlet weak var button6: VlKeynrButton!
+    @IBOutlet weak var button7: VlKeynrButton!
+    @IBOutlet weak var button8: VlKeynrButton!
+    @IBOutlet weak var buttonRandom: VlKeynrButton!
+    
+    var audioPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
-
-
 }
 
